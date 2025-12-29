c93710e7d36933068c6eb33e29ad8903ae1e59f4
Nisar k <nisardote@gmail.com>
2025-12-29 14:11:00 +0530

    Refactor challenge generation to use transformers pipeline

--- scripts/generate_challenge.py
+++ scripts/generate_challenge.py
@@ -1,24 +1,24 @@
 import os
-from huggingface_hub import InferenceClient
+from transformers import pipeline
 
 hf_api_key = os.environ['HF_API_KEY']
 challenge_file = os.environ['CHALLENGE_FILE']
 
-client = InferenceClient(token=hf_api_key)
+generator = pipeline(
+    "text-generation",
+    model="bigcode/starcoder",
+    use_auth_token=hf_api_key
+)
 
 prompt = (
     "Generate a concise coding challenge with a problem statement and "
     "solution code in JavaScript, Python, or SQL."
 )
 
-# Fix: use 'prompt' instead of 'inputs'
-response = client.text_generation(model="bigcode/starcoder", prompt=prompt, max_new_tokens=400)
+result = generator(prompt, max_new_tokens=400)
 
-# Hugging Face response may be a list of dicts or a string
-if isinstance(response, list) and "generated_text" in response[0]:
-    challenge_text = response[0]["generated_text"]
-else:
-    challenge_text = str(response)
+# result is a list of dicts with 'generated_text'
+challenge_text = result[0]['generated_text']
 
 with open(challenge_file, "w") as f:
     f.write(challenge_text)
